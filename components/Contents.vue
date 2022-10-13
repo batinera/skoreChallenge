5be52adb78cb1c245bb76d9e3aac03fca507c84e
batinera <batinileo@gmail.com>
2022-10-13 19:12:29 -0300

    first commit

---
+++ components/Contents.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="content">
+    <div class="content-inner">
+      <div class="content-text-wrap">
+        <article v-for="content in contents" :key="content.id">
+          <h1> {{ content.title }} </h1>
+        </article>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+const contents = gql`
+  query contents {
+    contents {
+      id
+      title
+      description
+      type
+      url
+      created_at
+      updated_at
+    }
+  }
+  `
+
+export default {
+  apollo: {
+    contents: {
+      query: contents,
+      prefetch: true
+    }
+  }
+}
+</script>
+
+<style src="../assets/style/contents.css">
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="index">
+    <section class="contents">
+      <Contents />
+    </section>
+  </main>
+</template>
+
+<script>
+import Contents from "../components/Contents.vue";
+
+export default {
+  name: 'index',
+  components: { Contents }
+}
+
+</script>
+
+<style src="../assets/style/index.css">
+
+</style>
